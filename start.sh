c878e3e4a73bf32a4ea09e70b663584d6269c144
Mohammad Abdi <mohammad_abdi76@yahoo.com>
2024-04-23 12:23:28 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -55,8 +55,8 @@ nameserver 1.1.1.1
 EOF
 
 if [ $COUNTRY = "IR" ]; then
-    export http_proxy='http://fodev.org:8118'    
-    export https_proxy='http://fodev.org:8118'
+    export http_proxy='http://wraygnbd:eqj5y20wjznk@proxy.chabokan-two.ir:6187'
+    export https_proxy='http://wraygnbd:eqj5y20wjznk@proxy.chabokan-two.ir:6187'
     echo -e "${GREEN}change server repo ...${NC}"
     sed -i 's/http:\/\/archive.ubuntu.com/http:\/\/ir.archive.ubuntu.com/g' /etc/apt/sources.list
     sed -i 's/http:\/\/security.ubuntu.com/http:\/\/ir.archive.ubuntu.com/g' /etc/apt/sources.list
