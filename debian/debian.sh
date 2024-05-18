14795fae1b0e2d7b53f69d18d44f3eafda3b2cd1
AliReza <107261014+AlirezaNY98@users.noreply.github.com>
2024-05-18 13:54:58 +0330

    update

--- debian/debian.sh
+++ debian/debian.sh
@@ -10,51 +10,6 @@ set -e
 # Eror handeling
 trap "echo -e '${RED}ERROR: Run the command again and select a different type of connection!${NC}'" ERR
 
-unset http_proxy
-unset https_proxy
-
-# Type of connection menu
-function choose_from_menu() {
-    local prompt="$1" outvar="$2"
-    shift
-    shift
-    local options=("$@") cur=0 count=${#options[@]} index=0
-    local esc=$(echo -en "\e") # cache ESC as test doesn't allow esc codes
-    printf "$prompt\n"
-    while true
-    do
-        # list all options (option list is zero-based)
-        index=0
-        for o in "${options[@]}"
-        do
-            if [ "$index" == "$cur" ]
-            then echo -e " >\e[7m$o\e[0m" # mark & highlight the current option
-            else echo "  $o"
-            fi
-            index=$(( $index + 1 ))
-        done
-        read -s -n3 key # wait for user to key in arrows or ENTER
-        if [[ $key == $esc[A ]] # up arrow
-        then cur=$(( $cur - 1 ))
-            [ "$cur" -lt 0 ] && cur=0
-        elif [[ $key == $esc[B ]] # down arrow
-        then cur=$(( $cur + 1 ))
-            [ "$cur" -ge $count ] && cur=$(( $count - 1 ))
-        elif [[ $key == "" ]] # nothing, i.e the read delimiter - ENTER
-        then break
-        fi
-        echo -en "\e[${count}A" # go up to the beginning to re-render
-    done
-    # export the selection to the requested output variable
-    printf -v $outvar "${options[$cur]}"
-}
-selections=(
-"Direct"
-"FOD"
-"Shecan-DNS"
-"403-DNS"
-)
-
 if [[ "$1" != '' ]]; then
     TOKEN=$1
 else
@@ -80,66 +35,14 @@ echo -e "${GREEN}add base dns ...${NC}"
     options timeout:1
     nameserver 8.8.8.8
     nameserver 1.1.1.1
-    EOF
-
-SERVER_IP=$(hostname -I | awk '{print $1}')
-IP_CHECK_URL="https://api.country.is/$SERVER_IP"
-CHECK_IP=$(curl -s "$IP_CHECK_URL")
-if echo "$CHECK_IP" | grep -q "\"error\""; then
-  echo -e "${RED} Error! IP address not found ${NC}"
-  exit 1
-fi
-
-COUNTRY=$(echo "$CHECK_IP" | grep -o -P '"country":"\K[^"]+' | tr -d \")
-
-echo -e "${GREEN}Server IP: ${SERVER_IP} ${NC}"
-echo -e "${GREEN}Server Country: ${COUNTRY} ${NC}"
+EOF
 
 echo -e "${GREEN}set Tehran Timezone ...${NC}"
 TZ=Asia/Tehran
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
-if [ "$COUNTRY" = "IR" ]; then
-    if [[ "$2" != '' ]]; then
-        TYPE_OF_CONNECT=$2
-    else
-        echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-Type of connections are used for connect your server to chabokan server
-assistant.You can choose from various connection types. If you encounter
-any issues, simply try running the command again with a different
-type of connection. For more detailed instructions, please refer to
-the Chabokan documentation:
-https://docs.chabokan.net/server-assistant/setup/
-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"      
-        choose_from_menu "Please select Type of Connect:" TYPE_OF_CONNECT "${selections[@]}"
-    fi
-
-    echo -e "${GREEN}Type of Connect: ${TYPE_OF_CONNECT} ${NC}" 
-    if [ $TYPE_OF_CONNECT = "FOD" ]; then
-        echo -e "${GREEN}set type of connect fod ...${NC}"
-        export http_proxy='http://fodev.org:8118'
-        export https_proxy='http://fodev.org:8118'
-    elif [ $TYPE_OF_CONNECT = "Shecan-DNS" ]; then
-        echo -e "${GREEN}add shecan dns ...${NC}"
-        rm /etc/resolv.conf
-        cat >/etc/resolv.conf <<EOF
-        options timeout:1
-        nameserver 178.22.122.100
-        nameserver 185.51.200.2
-        EOF
-    elif [ $TYPE_OF_CONNECT = "403-DNS" ]; then
-        echo -e "${GREEN}add 403 dns ...${NC}"
-        rm /etc/resolv.conf
-        cat >/etc/resolv.conf <<EOF
-        options timeout:1
-        nameserver 10.202.10.202
-        nameserver 10.202.10.102
-        EOF
-    fi
-fi
 
 echo -e "${GREEN}updating os ...${NC}"
-
 apt update -y
 
 echo -e "${GREEN}disable ipv6 ...${NC}"
@@ -194,7 +97,7 @@ mkdir -p /backups
 
 echo "0 3 * * * root bash /var/ch-manager/update_core.sh >> /dev/null 2>&1" > /etc/cron.d/update-core
 echo "0 6 * * * root git pull /var/server-connector >> /dev/null 2>&1" > /etc/cron.d/update-packages
-echo "0 7 * * * root bash /var/server-connector/packages.sh >> /dev/null 2>&1" > /etc/cron.d/update-packages
+echo "0 7 * * * root bash /var/server-connector/debian/packages.sh >> /dev/null 2>&1" > /etc/cron.d/update-packages
 service cron restart
 
 echo -e "${GREEN}installing node manager ....${NC}"
@@ -227,22 +130,18 @@ BASH_ENV=/.env
 " > /etc/cron.d/server-queue
 service cron restart
 
-cd /tmp
-wget https://raw.githubusercontent.com/chabokan/server-connector/main/vsftpd.conf -O vsftpd.conf
-wget https://raw.githubusercontent.com/chabokan/server-connector/main/sshd_config -O sshd_config
-
-cp ./vsftpd.conf /etc/vsftpd.conf
+cp /var/server-connector/configs/vsftpd.conf /etc/vsftpd.conf
 sed -i 's,\r,,;s, *$,,' /etc/vsftpd.conf
-cp ./sshd_config /etc/ssh/sshd_config
+cp /var/server-connector/configs/sshd_config /etc/ssh/sshd_config
 
 service ssh restart
 service sshd restart
 service vsftpd restart
 
-rm -rf ./vsftpd.conf
-rm -rf ./sshd_config
+rm -rf /var/server-connector/configs/vsftpd.conf
+rm -rf /var/server-connector/configs/sshd_config
 
-curl -s https://raw.githubusercontent.com/chabokan/server-connector/main/firewall.sh | bash
+bash /var/server-connector/configs/firewall.sh
 
 if [ $COUNTRY = "IR" ]; then
 echo -e "${GREEN}add shecan dns ...${NC}"
@@ -251,7 +150,7 @@ echo -e "${GREEN}add shecan dns ...${NC}"
     options timeout:1
     nameserver 178.22.122.100
     nameserver 185.51.200.2
-    EOF
+EOF
 fi
 
 sleep 15
--- start.sh
+++ start.sh
@@ -13,6 +13,110 @@ set -e
 # Eror handeling
 trap "echo -e '${RED}ERROR:Your operating system is not supported by this script.${NC}'" ERR
 
+unset http_proxy
+unset https_proxy
+
+# Type of connection menu
+function choose_from_menu() {
+    local prompt="$1" outvar="$2"
+    shift
+    shift
+    local options=("$@") cur=0 count=${#options[@]} index=0
+    local esc=$(echo -en "\e") # cache ESC as test doesn't allow esc codes
+    printf "$prompt\n"
+    while true
+    do
+        # list all options (option list is zero-based)
+        index=0
+        for o in "${options[@]}"
+        do
+            if [ "$index" == "$cur" ]
+            then echo -e " >\e[7m$o\e[0m" # mark & highlight the current option
+            else echo "  $o"
+            fi
+            index=$(( $index + 1 ))
+        done
+        read -s -n3 key # wait for user to key in arrows or ENTER
+        if [[ $key == $esc[A ]] # up arrow
+        then cur=$(( $cur - 1 ))
+            [ "$cur" -lt 0 ] && cur=0
+        elif [[ $key == $esc[B ]] # down arrow
+        then cur=$(( $cur + 1 ))
+            [ "$cur" -ge $count ] && cur=$(( $count - 1 ))
+        elif [[ $key == "" ]] # nothing, i.e the read delimiter - ENTER
+        then break
+        fi
+        echo -en "\e[${count}A" # go up to the beginning to re-render
+    done
+    # export the selection to the requested output variable
+    printf -v $outvar "${options[$cur]}"
+}
+selections=(
+"Direct"
+"FOD"
+"Shecan-DNS"
+"403-DNS"
+)
+
+
+SERVER_IP=$(hostname -I | awk '{print $1}')
+IP_CHECK_URL="https://api.country.is/$SERVER_IP"
+CHECK_IP=$(curl -s "$IP_CHECK_URL")
+if echo "$CHECK_IP" | grep -q "\"error\""; then
+  echo -e "${RED} Error! IP address not found ${NC}"
+  exit 1
+fi
+
+export COUNTRY=$(echo "$CHECK_IP" | grep -o -P '"country":"\K[^"]+' | tr -d \")
+
+echo -e "${GREEN}Server IP: ${SERVER_IP} ${NC}"
+echo -e "${GREEN}Server Country: ${COUNTRY} ${NC}"
+
+
+if [ "$COUNTRY" = "IR" ]; then
+    if [[ "$2" != '' ]]; then
+        TYPE_OF_CONNECT=$2
+    else
+        echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Type of connections are used for connect your server to chabokan server
+assistant.You can choose from various connection types. If you encounter
+any issues, simply try running the command again with a different
+type of connection. For more detailed instructions, please refer to
+the Chabokan documentation:
+https://docs.chabokan.net/server-assistant/setup/
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"      
+        choose_from_menu "Please select Type of Connect:" TYPE_OF_CONNECT "${selections[@]}"
+    fi
+
+    echo -e "${GREEN}Type of Connect: ${TYPE_OF_CONNECT} ${NC}" 
+    if [ $TYPE_OF_CONNECT = "FOD" ]; then
+      echo -e "${GREEN}set type of connect fod ...${NC}"
+      export http_proxy='http://fodev.org:8118'
+      export https_proxy='http://fodev.org:8118'
+    elif [ $TYPE_OF_CONNECT = "Shecan-DNS" ]; then
+      echo -e "${GREEN}add shecan dns ...${NC}"
+      rm /etc/resolv.conf
+      cat >/etc/resolv.conf <<EOF
+      options timeout:1
+      nameserver 178.22.122.100
+      nameserver 185.51.200.2
+    EOF
+    elif [ $TYPE_OF_CONNECT = "403-DNS" ]; then
+      echo -e "${GREEN}add 403 dns ...${NC}"
+      rm /etc/resolv.conf
+      cat >/etc/resolv.conf <<EOF
+      options timeout:1
+      nameserver 10.202.10.202
+      nameserver 10.202.10.102
+    EOF
+    fi
+fi
+
+
+cd /var
+git clone https://github.com/chabokan/server-connector.git
+
+
 # Check OS and set release variable
 if [[ -f /etc/os-release ]]; then
     source /etc/os-release
@@ -26,11 +130,8 @@ else
 fi
 echo "The OS is: $release"
 
-cd /var
-git clone https://github.com/chabokan/server-connector.git
-
 if [ "$release" = "ubuntu" ]; then
-    bash /var/server-connector/ubuntu.sh $1
+    bash /var/server-connector/debian/ubuntu.sh $1
 elif [ "$release" = "debian" ]; then
-    bash /var/server-connector/debian.sh $1
+    bash /var/server-connector/debian/debian.sh $1
 fi
